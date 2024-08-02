3fbf07dd3982c4aafd94ba0b2e15d169916ff1ab
Luka Jiang (柏學) <lukajiang1998@gmail.com>
2024-08-02 01:55:31 +0000

    simple interface

--- stockllm/analyzer/base_analyzer.py
+++ stockllm/analyzer/base_analyzer.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Any, Generator
+from typing import Any, Dict, Generator, Tuple
 
 
 class BaseAnalyzer(ABC):
@@ -21,7 +21,7 @@ class BaseAnalyzer(ABC):
 
     @abstractmethod
     def load_data(self) -> Generator:
-        """Load data with self.configs or others
+        """Load data from local or internet by self.configs or others
 
         Yields:
             Generator: Data with generator type, which can be generate/pop with get_current_data() method.
@@ -29,28 +29,40 @@ class BaseAnalyzer(ABC):
         pass
 
     @abstractmethod
-    def get_current_data(self) -> Any:
+    def get_current_data(self) -> Dict[str, Any]:
         """Yields batch of data by self.data_generator or others, which can be defined by configs.
 
         Returns:
-            Any: Batch of data.
+            Dict[str, Any]: Batch of data and its ground truth.
+                Ex.
+                    {
+                        "x": ["In celebration of Zoox’s 10th anniversary, NVIDIA founder, ...",
+                              "Since 2014, Zoox has been on a mission to create fully autonomous, ..."],
+                        "y": 937.6
+                    }
+                or
+                    {
+                        "x": [919.2, 905.9],
+                        "y": 937.6
+                    }
         """
+
         pass
 
     @abstractmethod
-    def predict_step(self, data: Any) -> float:
+    def predict_step(self, data: Any) -> Tuple[float]:
         """Use model/algorithms/gpt/rulebase or anything to get a predict score.
 
         Args:
             data (Any): Data from the output of self.get_current_data().
 
         Returns:
-            float: A predict score denote by
+            Tuple[float]: A score predicting whether a stock will rise or fall and its ground truth.
         """
-
         pass
 
-    def forward(self):
+    def __call__(self) -> float:
         current_data = self.get_current_data()
-        predict_results = self.predict_step(data=current_data)
-        return predict_results
+        predict_results = self.predict_step(data=current_data.get("x"))
+        ground_truth = current_data.get("y")
+        return predict_results, ground_truth
