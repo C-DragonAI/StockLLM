3fca752a4d6cfc0f34ab92b34d411b767b035f26
Luka Jiang (柏學) <lukajiang1998@gmail.com>
2024-08-06 09:04:00 +0000

    simple base object

--- stockllm/analyzer/base_analyzer.py
+++ stockllm/analyzer/base_analyzer.py
@@ -1,11 +1,18 @@
 from abc import ABC, abstractmethod
-from typing import Any, Dict, Generator, Tuple
+from typing import Any
 
 
 class BaseAnalyzer(ABC):
+    """BaseAnalyzer is a unified analysis interface, which can be extended to analyze various aspects of stocks.
+
+    Usage:
+        analyzer = Analyzer(configs_file = "path/to/your/configs")
+        prediction = analyzer.analysis()
+    """
+
     def __init__(self, configs_file: str) -> None:
         self.configs = self.load_configs(configs_file)
-        self.data_generator = self.load_data()
+        self.data = self.load_data()
 
     @abstractmethod
     def load_configs(self, configs_file: str) -> dict:
@@ -20,49 +27,19 @@ class BaseAnalyzer(ABC):
         pass
 
     @abstractmethod
-    def load_data(self) -> Generator:
+    def load_data(self) -> Any:
         """Load data from local or internet by self.configs or others
 
-        Yields:
-            Generator: Data with generator type, which can be generate/pop with get_current_data() method.
+        Returns:
+            Any: Data during the past N days.
         """
         pass
 
     @abstractmethod
-    def get_current_data(self) -> Dict[str, Any]:
-        """Yields batch of data by self.data_generator or others, which can be defined by configs.
+    def analysis(self) -> str:
+        """Use model/algorithms/gpt/rulebase or anything to get a prediction by self.data.
 
         Returns:
-            Dict[str, Any]: Batch of data and its ground truth.
-                Ex.
-                    {
-                        "x": ["In celebration of Zoox’s 10th anniversary, NVIDIA founder, ...",
-                              "Since 2014, Zoox has been on a mission to create fully autonomous, ..."],
-                        "y": 937.6
-                    }
-                or
-                    {
-                        "x": [919.2, 905.9],
-                        "y": 937.6
-                    }
-        """
-
-        pass
-
-    @abstractmethod
-    def predict_step(self, data: Any) -> Tuple[float]:
-        """Use model/algorithms/gpt/rulebase or anything to get a predict score.
-
-        Args:
-            data (Any): Data from the output of self.get_current_data().
-
-        Returns:
-            Tuple[float]: A score predicting whether a stock will rise or fall and its ground truth.
+            str: Summary of its aspect analysis and the prediction of the next day.
         """
         pass
-
-    def __call__(self) -> float:
-        current_data = self.get_current_data()
-        predict_results = self.predict_step(data=current_data.get("x"))
-        ground_truth = current_data.get("y")
-        return predict_results, ground_truth
